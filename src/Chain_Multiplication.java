dbb170396035c42330f8509544cbd637aeebfea2
JinWonLee95 <dlwlsdnjs456@naver.com>
2018-12-05 02:38:15 +0900

    구현 완료

--- src/Chain_Multiplication.java
+++ src/Chain_Multiplication.java
@@ -1,74 +1,95 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Chain_Multiplication {
-    static int[][] m = new int[6][6];
-    static int[][] s = new int[6][6];
+    static int[][] s;
+    static int[][] m;
 
-    public static int[][] matrix_Chain_Order(int[] p){
-        int n = p.length-1;
-        for(int i=1; i<=n; i++){
+    public static void matrix_Chain_Order(ArrayList<Integer> p) {
+        m = new int[p.size()][p.size()];
+        s = new int[p.size()][p.size()];
+        int n = p.size() - 1;
+        for (int i = 1; i <= n; i++) {
             m[i][i] = 0;
         }
 
-        for(int l=2; l<=n; l++){
-            for(int i=1; i<=n-1+1; i++){
-                int j = i+l-1;
+        for (int l = 2; l <= n; l++) {
+            for (int i = 1; i <= n - l + 1; i++) {
+                int j = i + l - 1;
                 m[i][j] = Integer.MAX_VALUE;
 
-                for(int k=i; k<=j-1; k++){
-                    int q = m[i][k]+m[k+1][j]+p[i-1]*p[k]*p[i];
+                for (int k = i; k <= j - 1; k++) {
+                    int q = m[i][k] + m[k + 1][j] + p.get(i - 1) * p.get(k) * p.get(j);
 
-                    if(q<m[i][j]){
+                    if (q < m[i][j]) {
                         m[i][j] = q;
                         s[i][j] = k;
                     }
                 }
             }
         }
-        System.out.println("---------------------------------------------");
-        for(int i=0; i<m.length; i++){
-            for(int j=0; j<n; j++){
-                System.out.println(m[i][j]+" ");
+        System.out.println("------------------------------------------------------------------");
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (i > j)
+                    m[i][j] = -1;
+                System.out.printf("%8d", m[i][j]);
             }
+            System.out.println();
         }
-        System.out.println("--------------------------------------------");
-        for(int i=0; i<m.length; i++){
-            for(int j=0; j<n; j++){
-                System.out.println(s[i][j]+" ");
-            }
-        }
-        System.out.println("--------------------------------------------");
 
-        return s;
+        System.out.println("-----------------------------------------------------------------");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i > j)
+                    s[i][j] = -1;
+                System.out.printf("%8d", s[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------------------------------------------------------");
+
     }
 
-    public static void print_optimal_parens(int[][] s, int i, int j){
-        if(i==j){
-            System.out.println("A"+i+" ");
+    public static void print_optimal_parens(int[][] s, int i, int j) {
+        if (i == j) {
+            System.out.print(" A" + (i) + " ");
         } else {
-            System.out.print("( ");
-            print_optimal_parens(s,i,s[i][j]);
-            print_optimal_parens(s, s[i][j]+1, j);
-            System.out.println(")");
+            System.out.print(" ( ");
+            print_optimal_parens(s, i, s[i][j]);
+            print_optimal_parens(s, s[i][j] + 1, j);
+            System.out.print(" ) ");
         }
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] p = new int[6];
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> p = new ArrayList<>();
+        int i = 1;
 
-        for(int i=1; i<=6; i++){
-            System.out.print("A("+i+") = ");int row = Integer.parseInt(br.readLine());
-            System.out.print(" x ");
-            int column = Integer.parseInt(br.readLine());
-
-            p[i-1] = row;
+        while (true) {
+            System.out.print(i + "번째 수: ");
+            int temp = sc.nextInt();
+            if (temp == 0) {
+                break;
+            }
+            p.add(temp);
+            i++;
         }
 
-        int[][] s = matrix_Chain_Order(p);
-        print_optimal_parens(s,0,0);
+        System.out.println("=========================================================================================");
+        for (int k = 1; k < i - 1; k++) {
+            System.out.println("A(" + k + ") = " + p.get(k - 1) + " x " + p.get(k));
+        }
+
+        matrix_Chain_Order(p);
+
+        System.out.println("Optimal Solution : "+m[1][m.length-1]);
+        System.out.print("Optimal Parens: "); print_optimal_parens(s, 1, i - 2);
+
 
     }
 }
